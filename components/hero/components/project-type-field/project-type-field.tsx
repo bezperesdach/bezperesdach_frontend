ff6686a5d4a86b5f7332941cc1262e0182d21075
maslomeister <maslomeister@gmail.com>
2022-10-26 22:14:22 +0400

    feat: hero component

---
+++ components/hero/components/project-type-field/project-type-field.tsx
@@ -0,0 +1,79 @@
+import React, { useId } from "react";
+import { FieldProps } from "formik";
+import Select from "react-select";
+import { Options, OnChangeValue, StylesConfig } from "react-select";
+
+const customControlStyles: React.CSSProperties = {
+  borderRadius: 0,
+};
+
+const selectStyle: StylesConfig<Option | Option[] | string, boolean> = {
+  menu: (provided, state) => {
+    // const { selectProps } = state;
+
+    return {
+      ...provided,
+      ...customControlStyles,
+    };
+  },
+};
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface CustomSelectProps extends FieldProps {
+  options: Options<Option>;
+  isMulti?: boolean;
+  className?: string;
+  placeholder?: string;
+  borderRadius?: number;
+}
+
+export const ProjectTypeSelect = ({
+  className,
+  placeholder,
+  borderRadius = 25,
+  field,
+  form,
+  options,
+  isMulti = false,
+}: CustomSelectProps) => {
+  const onChange = (option: OnChangeValue<Option | Option[] | string, boolean>) => {
+    form.setFieldValue(field.name, isMulti ? (option as Option[]).map((item: Option) => item.value) : (option as Option).value);
+  };
+
+  const getValue = () => {
+    if (options) {
+      return isMulti
+        ? options.filter((option) => field.value.indexOf(option.value) >= 0)
+        : options.find((option) => option.value === field.value);
+    } else {
+      return isMulti ? [] : "";
+    }
+  };
+
+  return (
+    <Select
+      className={className}
+      name={field.name}
+      value={getValue()}
+      onChange={onChange}
+      placeholder={placeholder}
+      options={options}
+      isMulti={isMulti}
+      theme={(theme) => ({
+        ...theme,
+        borderRadius,
+        fontFamily: "Roboto",
+      })}
+      components={{
+        IndicatorSeparator: () => null,
+      }}
+      instanceId={useId()}
+      styles={selectStyle}
+      noOptionsMessage={({ inputValue }) => (!inputValue ? "" : "Тип не найден")}
+    />
+  );
+};
--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,5 +1,161 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-export const hero = () => {
-  return <div>hero</div>;
+import { ProjectTypeSelect } from "./components/project-type-field/project-type-field";
+
+import styles from "./hero.module.css";
+import { Button } from "../button/button";
+
+interface IFields {
+  projectType: string;
+  projectName: string;
+  dueDate: string;
+  originality: string;
+  description: string;
+}
+
+const nextWeek = () => {
+  const now = new Date();
+  const twoWeeks = new Date(now.getTime() + 14 * 24 * 60 * 60 * 1000);
+  return twoWeeks;
+};
+
+const initialValue: IFields = {
+  projectType: "",
+  projectName: "",
+  dueDate: nextWeek().toLocaleDateString("en-CA"),
+  originality: "70%",
+  description: "",
+};
+
+const RequestProjectSchema = Yup.object().shape({
+  projectType: Yup.string().required("Обязательное поле"),
+  projectName: Yup.string().required("Обязательное поле"),
+  dueDate: Yup.string().required("Обязательное поле"),
+  originality: Yup.string().required("Обязательное поле"),
+});
+
+const options = [
+  { value: "Вкр", label: "Вкр" },
+  { value: "Дипломная работа", label: "Дипломная работа" },
+  { value: "Хд", label: "Хд" },
+];
+
+export const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles["hero-title"]}>Онлайн-платформа для помощи в учебе</h1>
+      <Formik
+        initialValues={initialValue}
+        validationSchema={RequestProjectSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting, setFieldValue, values }) => (
+          <Form className={styles.form} noValidate>
+            <div className={styles["form-item"]}>
+              <label className={styles.label}>Тип работы</label>
+              <Field
+                name="projectType"
+                options={options}
+                component={ProjectTypeSelect}
+                borderRadius={15}
+                placeholder="Выберите тип"
+                isMulti={false}
+                disabled={isSubmitting}
+              />
+
+              <ErrorMessage className={styles["error-label"]} name="projectType" component="div" />
+            </div>
+
+            <div className={styles["form-item"]}>
+              <label className={styles.label}>Название работы</label>
+              <div className={styles["input-container"]}>
+                <Field
+                  className={styles.input}
+                  type="text"
+                  name="projectName"
+                  placeholder="Как должна называться ваша работа"
+                  disabled={isSubmitting}
+                />
+              </div>
+              <ErrorMessage className={styles["error-label"]} name="projectName" component="div" />
+            </div>
+
+            <div className={styles["date-orig"]}>
+              <div className={styles["form-item"]}>
+                <label className={styles.label}>Дата сдачи</label>
+                <div className={styles["input-container"]}>
+                  <Field
+                    className={styles.input}
+                    type="date"
+                    name="dueDate"
+                    placeholder="Когда нужно сдать работу"
+                    disabled={isSubmitting}
+                  />
+                </div>
+                <ErrorMessage className={styles["error-label"]} name="dueDate" component="div" />
+              </div>
+
+              <div className={styles["form-item"]}>
+                <label className={styles.label}>Оригинальность</label>
+                <div className={styles["input-container"]}>
+                  <Field
+                    className={styles.input}
+                    type="text"
+                    pattern="\d*"
+                    name="originality"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      const value = e.target.value.trim().replace(/[^0-9]/gi, "");
+                      if (value === "" || value === "0") {
+                        return setFieldValue("originality", "");
+                      }
+                      if (value.length >= 3) {
+                        return;
+                      }
+                      return setFieldValue("originality", `${value}%`);
+                    }}
+                    onKeyDown={(e: React.KeyboardEvent) => {
+                      if (e.key === "Backspace") {
+                        setFieldValue("originality", values.originality.slice(0, -1));
+                      }
+                    }}
+                    placeholder="Оригинальность"
+                    disabled={isSubmitting}
+                    data-value="originality"
+                  />
+                </div>
+                <ErrorMessage className={styles["error-label"]} name="originality" component="div" />
+              </div>
+            </div>
+
+            <div className={styles["form-item"]}>
+              <label className={styles.label}>Дополнительное описание</label>
+              <div className={styles["input-container"]}>
+                <Field
+                  className={styles.input}
+                  type="text"
+                  component="textarea"
+                  rows="7"
+                  name="description"
+                  placeholder="Небольшое описание работы"
+                  disabled={isSubmitting}
+                />
+              </div>
+              <ErrorMessage className={styles["error-label"]} name="description" component="div" />
+            </div>
+
+            <Button type="submit" disabled={isSubmitting} style={{ alignSelf: "center" }}>
+              Отправить запрос
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };

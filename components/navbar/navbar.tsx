7d670dd317c39a4f9179f0642222ee0cd726ad54
maslomeister <maslomeister@gmail.com>
2022-10-29 01:55:29 +0400

    refactor: import logo

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Image from "next/image";
 
+import logo from "public/logo.svg";
+
 import styles from "./navbar.module.css";
 
-// type Props = {};
-
-export const Navbar = (/* props: Props */) => {
+export const Navbar = () => {
   return (
     <header className={styles.navbar}>
-      <Image src="/logo.svg" height={100} priority={true} width={320} alt="logo" />
+      <Image src={logo} height={100} priority={true} width={320} alt="logo" />
     </header>
   );
 };

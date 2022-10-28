fe70c9a065093c37258d886ec7052f41bb10eb1c
maslomeister <maslomeister@gmail.com>
2022-10-29 01:54:49 +0400

    feat: button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -6,14 +6,18 @@ type Props = {
   children: string;
   onClick?: () => void;
   disabled?: boolean;
+  loading?: boolean;
   type?: "button" | "submit" | "reset";
   style?: React.CSSProperties;
 };
 
-export const Button: React.FC<Props> = ({ children, type, onClick, disabled, style }) => {
+export const Button: React.FC<Props> = ({ children, type, onClick, loading, disabled, style }) => {
+  const buttonLoading = () => {
+    return loading ? <span className={styles["button-loading"]}></span> : <span>{children}</span>;
+  };
   return (
-    <button className={styles.button + " " + styles["button-gradient"]} type={type} onClick={onClick} disabled={disabled} style={style}>
-      {children}
+    <button className={`${styles.button} ${styles["button-gradient"]}`} type={type} onClick={onClick} disabled={disabled} style={style}>
+      {buttonLoading()}
     </button>
   );
 };

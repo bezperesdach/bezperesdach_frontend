bfd1daf8e0f6da002c01320805fa3bf79a7ebead
maslomeister <maslomeister@gmail.com>
2022-10-31 16:55:48 +0400

    feat: error boundary component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
+import ErrorBoundary from "../components/error-boundary/error-boundary";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />;
+    </ErrorBoundary>
+  );
 }

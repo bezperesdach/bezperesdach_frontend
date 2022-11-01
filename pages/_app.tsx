20322bb69983f8c2ba82819f9b7a21927468952b
maslomeister <maslomeister@gmail.com>
2022-11-01 13:56:44 +0400

    fix: yandex metrica

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import ErrorBoundary from "../components/error-boundary/error-boundary";
 import type { AppProps } from "next/app";
 
-import { WithYandexMetrika } from "../hocs/withYandexMetrica";
-
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <WithYandexMetrika>
-        <Component {...pageProps} />
-      </WithYandexMetrika>
+      <Component {...pageProps} />
     </ErrorBoundary>
   );
 }

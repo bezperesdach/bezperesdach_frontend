a6f7ec63a2706e9f1084f53542d1a47a96a12e55
maslomeister <maslomeister@gmail.com>
2022-11-04 16:36:06 +0400

    feat: propType based on url query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,38 @@
 import Head from "next/head";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next/types";
 import { Guarantees } from "../components/home/guarantees/guarantees";
 import { Hero } from "../components/home/hero/hero";
 import { Services } from "../components/home/services/services";
 import { About } from "../components/home/about/about";
 import { Represents } from "../components/home/represents/represents";
 import { Preparation } from "../components/home/preparation/preparation";
+import { typeOptionsOrder } from "../utils/form/values";
 
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ pt }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Безпересдач</title>
-        <meta name="description" content="Онлайн-платформа для помощи в обучении" />
         <meta property="og:site_name" content="Безпересдач" />
-        <meta property="og:description" content="Онлайн-платформа для помощи в обучении" />
+        {pt ? (
+          <>
+            <meta name="description" content={typeOptionsOrder.get(pt)} />
+            <meta property="og:description" content={typeOptionsOrder.get(pt)} />
+          </>
+        ) : (
+          <>
+            <meta name="description" content="Онлайн-платформа для помощи в обучении" />
+            <meta property="og:description" content="Онлайн-платформа для помощи в обучении" />
+          </>
+        )}
         <meta property="og:image" content="/og_logo.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Hero />
+        <Hero projectType={pt} />
         <About />
         <Services />
         <Guarantees />
@@ -31,3 +42,11 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { pt } = context.query;
+
+  return {
+    props: { pt },
+  };
+};

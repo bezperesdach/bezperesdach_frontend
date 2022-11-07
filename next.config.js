2636a2760cdf678db36bf3f7d005271c16d67ba6
maslomeister <maslomeister@gmail.com>
2022-11-07 17:25:14 +0400

    feat: bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  productionBrowserSourceMaps: process.env.NODE_ENV === "development" ? true : false,
+};
 
-module.exports = nextConfig
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withBundleAnalyzer];
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...defaultConfig, ...nextConfig });
+};

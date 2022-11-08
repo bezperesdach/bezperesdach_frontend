e9f698e7a2fd779b8d77d050385d220d710d6f0f
maslomeister <maslomeister@gmail.com>
2022-11-08 15:24:34 +0400

    feat: sidebar lazy loading

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Turn as Hamburger } from "hamburger-react";
-import Portal from "../portal/portal";
-import { motion, AnimatePresence } from "framer-motion";
+
+const DynamicSidebar = dynamic(() => import("./components/sidebar").then((mod) => mod.Sidebar));
 
 import logo from "public/logo.svg";
 import mini_logo from "public/mini_logo.png";
@@ -36,31 +37,7 @@ export const Navbar = () => {
           Цены и Услуги
         </Link>
       </div>
-      <AnimatePresence>
-        {isOpen && (
-          <Portal>
-            <motion.div
-              initial={{ x: "100%" }}
-              animate={{ x: 0 }}
-              exit={{ x: "100%", transition: { duration: 0.15 } }}
-              transition={{ type: "ease-in-out", duration: 0.25 }}
-              className={styles["sidebar-overlay"]}
-            >
-              <Link href="/" className={styles.link} onClick={closeMenu} style={router.pathname == "/" ? { fontWeight: "bold" } : {}}>
-                Главная
-              </Link>
-              <Link
-                href="/price"
-                className={styles.link}
-                onClick={closeMenu}
-                style={router.pathname == "/price" ? { fontWeight: "bold" } : {}}
-              >
-                Цены и Услуги
-              </Link>
-            </motion.div>
-          </Portal>
-        )}
-      </AnimatePresence>
+      <DynamicSidebar isOpen={isOpen} router={router} closeMenu={closeMenu} />
     </header>
   );
 };

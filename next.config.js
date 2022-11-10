c72a8947a64da3369ceb48253d3905fd724015fa
maslomeister <maslomeister@gmail.com>
2022-11-10 19:09:08 +0400

    feat: stylelint

--- next.config.js
+++ next.config.js
@@ -3,11 +3,17 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const StylelintPlugin = require("stylelint-webpack-plugin"); // line to add
+
 /** @type {import('next').NextConfig} */
 let nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: process.env.NODE_ENV === "development" ? true : false,
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
 };
 
 module.exports = (_phase, { defaultConfig }) => {

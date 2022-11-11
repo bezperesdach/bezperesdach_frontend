4465db66f53d76d709d92434db0b2b16f8c26341
salavat-nasyrov <i@snasyrov.ru>
2022-11-12 01:53:35 +0200

    feat: new block Guarantes

--- components/home-page/guarantees/guarantees.tsx
+++ components/home-page/guarantees/guarantees.tsx
@@ -1,22 +1,29 @@
 import React from "react";
+import Image from "next/image";
 import { Card } from "./components/card/card";
+import main from "/public/images/guarantees/1.svg";
+import one from "/public/images/guarantees/2.svg";
+import two from "/public/images/guarantees/2.svg";
+import three from "/public/images/guarantees/2.svg";
 
 import styles from "./guarantees.module.css";
 
 export const Guarantees = () => {
   return (
-    <section className={styles.guarantees}>
-      <Card title="Доработки - бесплатно">
-        <p>
-          Гарантийный срок — <span className={styles.bold_blue}>до 30 дней</span>. В этот период вы можете обратиться за бесплатными
-          доработками
-        </p>
-      </Card>
-      <Card title="Онлайн-поддержка до самой защиты">
-        <p>
-          Мы ответим на любой вопрос <span className={styles.bold_blue}>24/7</span> по почте
-        </p>
-      </Card>
+    <section className={`${styles.guarantees_text} ${styles.colored_background}`}>
+      <h2>ПРОЦЕСС ЗАКАЗА</h2>
+      <div className={styles.guarantees}>
+        <div className={styles.guarantees_elements}>
+          <Image className={styles.images} src={main} alt="guarantees" />
+        </div>
+
+        <div className={styles.cards}>
+          <Card description={"Быстрый поиск исполнителей"} img={one}></Card>
+          <Card description={"Строгий отбор клиентов"} img={two}></Card>
+          <Card description={"Общение с экспертом осуществялет администратор"} img={three}></Card>
+          <Card description={"Цены ниже, чем у посредников"} img={three}></Card>
+        </div>
+      </div>
     </section>
   );
 };

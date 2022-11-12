00917ba01aaa932a1fae08ee2af0ef7d7b512cc6
salavat-nasyrov <i@snasyrov.ru>
2022-11-13 01:10:50 +0200

    fix: add new text

--- components/home-page/guarantees/guarantees.tsx
+++ components/home-page/guarantees/guarantees.tsx
@@ -11,17 +11,19 @@ import styles from "./guarantees.module.css";
 export const Guarantees = () => {
   return (
     <section className={`${styles.guarantees_text} ${styles.colored_background}`}>
-      <h2>ПРОЦЕСС ЗАКАЗА</h2>
+      <h2>
+        ПРОСТОЙ <br></br>ПРОЦЕСС ЗАКАЗА
+      </h2>
       <div className={styles.guarantees}>
         <div className={styles.guarantees_elements}>
           <Image className={styles.images} src={main} alt="guarantees" />
         </div>
 
         <div className={styles.cards}>
-          <Card description={"Быстрый поиск исполнителей"} img={one}></Card>
-          <Card description={"Строгий отбор клиентов"} img={two}></Card>
-          <Card description={"Общение с экспертом осуществялет администратор"} img={three}></Card>
-          <Card description={"Цены ниже, чем у посредников"} img={three}></Card>
+          <Card description={"Оставляете заявку на сайте через форму"} img={one}></Card>
+          <Card description={"Администратор свяжется с вами в течение 15 минут"} img={two}></Card>
+          <Card description={"После уточнения деталей заказ будет передан исполнителю"} img={three}></Card>
+          <Card description={"Вы получите работу в указанные сроки"} img={three}></Card>
         </div>
       </div>
     </section>

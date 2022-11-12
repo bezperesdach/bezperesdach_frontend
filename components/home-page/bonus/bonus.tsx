b3e8be938585dfa53a2c1527d02e64957447067a
salavat-nasyrov <i@snasyrov.ru>
2022-11-12 23:53:26 +0200

    fix: add href

--- components/home-page/bonus/bonus.tsx
+++ components/home-page/bonus/bonus.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import left from "/public/images/bonus/left.svg";
 import right from "/public/images/bonus/right.svg";
@@ -16,9 +17,11 @@ export const Bonus = () => {
           Скидка 10% <br></br> на первый заказ
         </h2>
         <p>Пока ты думаешь - другие заказывают</p>
-        <Button backgroundColor="#0b73fe" color="white">
-          Заказать работу
-        </Button>
+        <Link href="/order">
+          <Button backgroundColor="#0b73fe" color="white">
+            Заказать работу
+          </Button>
+        </Link>
       </div>
       <Image className={styles.images_right} src={right} alt="right" />
     </section>

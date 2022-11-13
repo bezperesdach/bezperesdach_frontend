8474cd8c448d0f8d4ec298e585cc444c4e8024b1
salavat-nasyrov <i@snasyrov.ru>
2022-11-13 21:22:25 +0200

    fix: edit href

--- components/home-page/contact/components/card/card.tsx
+++ components/home-page/contact/components/card/card.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./card.module.css";
+import Link from "next/link";
 
 type Props = {
   description: string;
   img: string;
+  url: string;
 };
 
-export const Card = ({ description, img }: Props) => {
+export const Card = ({ description, img, url }: Props) => {
   return (
     <div className={styles.card_style}>
       <Image className={styles.image} src={img} alt="service" />
       <div className={styles.card_text}>
-        <p>{description}</p>
+        <p>
+          <Link href={url}>{description}</Link>
+        </p>
       </div>
     </div>
   );
--- components/home-page/contact/contact.tsx
+++ components/home-page/contact/contact.tsx
@@ -6,6 +6,7 @@ import telegram from "/public/images/contact/telegram.svg";
 import mail from "/public/images/contact/mail.svg";
 import whats from "/public/images/contact/whats.svg";
 import instagram from "/public/images/contact/instagram.svg";
+import vk from "/public/images/contact/vk.svg";
 
 import styles from "./contact.module.css";
 
@@ -19,10 +20,14 @@ export const Contact = () => {
         <Image className={styles.images} src={maps} alt="contact" />
 
         <div className={styles.cards}>
-          <Card description={"@bezperesdach"} img={telegram}></Card>
-          <Card description={"help@bezperesdach.ru"} img={mail}></Card>
-          <Card description={"+7-901-281-90-47"} img={whats}></Card>
-          <Card description={"@bezperesdach"} img={instagram}></Card>
+          <Card url="https://t.me/bezperesdach_official" description={"@bezperesdach_official"} img={telegram}></Card>
+          <Card
+            url="mailto:help@bezperesdach.ru?subject=%D0%9F%D0%BE%D0%BC%D0%BE%D0%B3%D0%B8%D1%82%D0%B5%20%D0%BC%D0%BD%D0%B5"
+            description={"help@bezperesdach.ru"}
+            img={mail}
+          ></Card>
+          {/* <Card url="https://t.me/bezperesdach_official" description={"+7-901-281-90-47"} img={whats}></Card> */}
+          <Card url="https://vk.com/bezperesdach_help" description={"@bezperesdach_help"} img={vk}></Card>
         </div>
       </div>
     </section>

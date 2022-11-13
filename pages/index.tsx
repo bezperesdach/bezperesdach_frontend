ee292d792127137dc8d9838b1721f2ea4efa4927
maslomeister <maslomeister@gmail.com>
2022-11-13 15:31:19 +0400

    refactor: code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import dynamic from "next/dynamic";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next/types";
-
 import Layout from "../components/layout/layout";
 import { Main } from "../components/home-page/main/main";
 import { Service } from "../components/home-page/service/service";
@@ -10,27 +7,16 @@ import { Work } from "../components/home-page/work/work";
 import { Bonus } from "../components/home-page/bonus/bonus";
 import { Contact } from "../components/home-page/contact/contact";
 
-import { typeOptionsOrder } from "../utils/form/new-order-form";
-
 import styles from "../styles/Home.module.css";
 
-export default function Home({ pt }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home() {
   return (
     <Layout>
       <Head>
         <title>Безпересдач</title>
         <meta property="og:site_name" content="Безпересдач" />
-        {pt ? (
-          <>
-            <meta name="description" content={typeOptionsOrder.get(pt)} />
-            <meta property="og:description" content={typeOptionsOrder.get(pt)} />
-          </>
-        ) : (
-          <>
-            <meta name="description" content="Онлайн-платформа для помощи в обучении" />
-            <meta property="og:description" content="Онлайн-платформа для помощи в обучении" />
-          </>
-        )}
+        <meta name="description" content="Онлайн-платформа для помощи в обучении" />
+        <meta property="og:description" content="Онлайн-платформа для помощи в обучении" />
         <meta property="og:image" content="/og_logo.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -44,13 +30,3 @@ export default function Home({ pt }: InferGetServerSidePropsType<typeof getServe
     </Layout>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  context.res.setHeader("Cache-Control", "public, s-maxage=31536000, stale-while-revalidate=59");
-
-  const { pt } = context.query;
-
-  return {
-    props: { pt: pt ?? null },
-  };
-};

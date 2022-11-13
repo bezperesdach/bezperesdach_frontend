db0975320b6b3614e095ce774034c20cabcdcff2
salavat-nasyrov <i@snasyrov.ru>
2022-11-13 21:20:12 +0200

    feat: new component Advantages

---
+++ components/work-page/advantages/advantages.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import { Card } from "./components/card/card";
+import main from "/public/images/advantages/main.svg";
+import one from "/public/images/advantages/one.svg";
+import two from "/public/images/advantages/two.svg";
+import three from "/public/images/advantages/three.svg";
+import four from "/public/images/advantages/four.svg";
+
+import styles from "./advantages.module.css";
+
+export const Advantages = () => {
+  return (
+    <section className={`${styles.advantages} ${styles.colored_background}`}>
+      <Image className={styles.images} src={main} alt="advantages" />
+      <div className={styles.cards}>
+        <h2>ЗАРАБАТЫВАЙ С БЕЗПЕРЕСДАЧ ОТ 40 ТЫСЯЧ РУБЛЕЙ</h2>
+        <Card description={"Мы выплачиваем 100% от заказа вам на карту"} img={one}></Card>
+        <Card description={"Работа полностью удаленная"} img={two}></Card>
+        <Card description={"Мы постоянно на связи с вами для решения всех вопросов"} img={three}></Card>
+        <Card description={"Задачи выдаются в зависимости от вашей квалификации"} img={four}></Card>
+      </div>
+    </section>
+  );
+};

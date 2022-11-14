804774e5494a4401d1b4f97b23b05a54e7222c4e
maslomeister <maslomeister@gmail.com>
2022-11-14 19:48:26 +0400

    refactor: scroll to top

--- components/scroll-to-top-button/scroll-to-top-button.tsx
+++ components/scroll-to-top-button/scroll-to-top-button.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
-type Props = {
-  showAfter: number;
-};
-
 import styles from "./scroll-to-top-button.module.css";
 
 export const ScrollTopButton = () => {
   return (
     <div className={styles.scrolltop_container}>
       <div className={styles.scrolltop_wrap}>
-        <a href="#" role="button" aria-label="Scroll to top">
+        <a role="button" aria-label="Scroll to top" onClick={() => window.scrollTo(0, 0)}>
           <svg viewBox="0 0 48 48" width="48" height="48px" xmlns="http://www.w3.org/2000/svg">
             <path id={styles.scrolltop_bg} d="M0 0h48v48h-48z"></path>
             <path id={styles.scrolltop_arrow} d="M14.83 30.83l9.17-9.17 9.17 9.17 2.83-2.83-12-12-12 12z"></path>

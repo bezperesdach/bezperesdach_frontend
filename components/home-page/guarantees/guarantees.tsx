649b56a27ccc184df4d944f0e58e4b28db49ec39
maslomeister <maslomeister@gmail.com>
2022-11-14 19:36:56 +0400

    fixed: flex container for mobile and tablet

--- components/home-page/guarantees/guarantees.tsx
+++ components/home-page/guarantees/guarantees.tsx
@@ -10,21 +10,19 @@ import styles from "./guarantees.module.css";
 
 export const Guarantees = () => {
   return (
-    <section className={`${styles.guarantees_text} ${styles.colored_background}`}>
-      <h2>
-        ПРОСТОЙ <br></br>ПРОЦЕСС ЗАКАЗА
-      </h2>
-      <div className={styles.guarantees}>
-        <div className={styles.guarantees_elements}>
-          <Image className={styles.images} src={main} alt="guarantees" />
-        </div>
+    <section className={`${styles.guarantees} ${styles.colored_background}`}>
+      <div className={styles.cards}>
+        <h2>
+          ПРОСТОЙ <br></br>ПРОЦЕСС ЗАКАЗА
+        </h2>
+        <Card description={"Оставляете заявку на сайте через форму"} img={one}></Card>
+        <Card description={"Администратор свяжется с вами в течение 15 минут"} img={two}></Card>
+        <Card description={"После уточнения деталей заказ будет передан исполнителю"} img={three}></Card>
+        <Card description={"Вы получите работу в указанные сроки"} img={three}></Card>
+      </div>
 
-        <div className={styles.cards}>
-          <Card description={"Оставляете заявку на сайте через форму"} img={one}></Card>
-          <Card description={"Администратор свяжется с вами в течение 15 минут"} img={two}></Card>
-          <Card description={"После уточнения деталей заказ будет передан исполнителю"} img={three}></Card>
-          <Card description={"Вы получите работу в указанные сроки"} img={three}></Card>
-        </div>
+      <div className={styles.image_container}>
+        <Image className={styles.image} src={main} alt="guarantees" />
       </div>
     </section>
   );

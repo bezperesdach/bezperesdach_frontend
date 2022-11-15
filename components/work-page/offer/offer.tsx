928364989ec6f3eaf8f51fdd0a055f41edd94c95
maslomeister <maslomeister@gmail.com>
2022-11-15 17:21:14 +0400

    refactor: no duplicate code

--- components/work-page/offer/offer.tsx
+++ components/work-page/offer/offer.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import { Card } from "./components/card/card";
-import main from "/public/images/offer/main.svg";
-import one from "/public/images/offer/point.svg";
-import two from "/public/images/offer/two.svg";
-import three from "/public/images/offer/three.svg";
-import four from "/public/images/offer/four.svg";
+import { ImageWithText } from "../../image-with-text/image-with-text";
+import Main from "/public/images/offer/main.webp";
+import FallbackMain from "/public/images/offer/fallback-main.png";
+import One from "/public/images/offer/point.webp";
+import FallbackOne from "public/images/offer/fallback-point.png";
 
 import styles from "./offer.module.css";
 
@@ -13,16 +12,27 @@ export const Offer = () => {
   return (
     <section className={`${styles.offer} ${styles.colored_background}`}>
       <div className={styles.image_container}>
-        <Image className={styles.image} src={main} alt="advantages" />
+        <Image
+          src={Main}
+          placeholder="blur"
+          className={styles.image}
+          alt="map"
+          onError={(e) => (e.currentTarget.src = FallbackMain.src)}
+        />
       </div>
       <div className={styles.cards}>
         <h2>
           УСЛОВИЯ <br></br> ДЛЯ АВТОРОВ
         </h2>
-        <Card description={"Высшее образование (студенты старших курсов)*"} img={one}></Card>
-        <Card description={"Наличие компьютера с выходом в интернет"} img={one}></Card>
-        <Card description={"Уверенные знания в выбранной квалификации работ"} img={one}></Card>
-        <Card description={"Портфолио из различных работ"} img={one}></Card>
+        <ImageWithText description={"Высшее образование (студенты старших курсов)*"} img={One} fallbackImg={FallbackOne} alt="point" />
+        <ImageWithText description={"Наличие компьютера с выходом в интернет"} img={One} fallbackImg={FallbackOne} alt="point" />
+        <ImageWithText
+          description={"Уверенные знания в выбранной квалификации работ"}
+          img={One}
+          fallbackImg={FallbackOne}
+          alt="point"
+        />
+        <ImageWithText description={"Портфолио из различных работ"} img={One} fallbackImg={FallbackOne} alt="point" />
       </div>
     </section>
   );

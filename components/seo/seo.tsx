87a4e6fe33fba3a695910e06336ac9f3a1afe253
maslomeister <maslomeister@gmail.com>
2022-11-16 22:49:02 +0400

    refactor: og type optional

--- components/seo/seo.tsx
+++ components/seo/seo.tsx
@@ -4,11 +4,12 @@ interface Props {
   title: string; //max length 45 characters
   description: string; //max length 135 desktop/ 100 mobile
   url: string;
+  type?: string;
   keywords: string; //max length 255 characters
   children?: React.ReactNode;
 }
 
-export const SEO = ({ title, description, url, keywords, children }: Props) => {
+export const SEO = ({ title, description, url, keywords, type, children }: Props) => {
   return (
     <Head>
       <title>{title}</title>
@@ -16,7 +17,7 @@ export const SEO = ({ title, description, url, keywords, children }: Props) => {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta name="keywords" content={keywords} />
-      <meta property="og:type" content="website" />
+      <meta property="og:type" content={type ?? "website"} />
       <meta name="og:locale" content="ru_RU" />
       {children}
     </Head>

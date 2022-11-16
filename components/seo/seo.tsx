013fb8c4615ed6535c48e029f45d1816da1a040b
maslomeister <maslomeister@gmail.com>
2022-11-16 22:29:31 +0400

    feat: seo component

---
+++ components/seo/seo.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+interface Props {
+  title: string; //max length 45 characters
+  description: string; //max length 135 desktop/ 100 mobile
+  url: string;
+  keywords: string; //max length 255 characters
+  children?: React.ReactNode;
+}
+
+export const SEO = ({ title, description, url, keywords, children }: Props) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta name="keywords" content={keywords} />
+      <meta property="og:type" content="website" />
+      <meta name="og:locale" content="ru_RU" />
+      {children}
+    </Head>
+  );
+};

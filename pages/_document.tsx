7e15515756a0bd1bbe353767ab90bfe72af7ea1a
maslomeister <maslomeister@gmail.com>
2022-11-19 18:50:54 +0400

    refactor: fixed hrefs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ export default function Document() {
     <Html lang="ru">
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png?v=2" />
-        <link rel="icon" type="image/x-icon" href="assets/favicon/favicon.ico?v=2" />
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png?v=2" />
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png?v=2" />
-        <link rel="manifest" href="assets/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#0b73fe" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png?v=2" />
+        <link rel="icon" type="image/x-icon" href="/assets/favicon/favicon.ico?v=2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=2" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=2" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#0b73fe" />
         <meta name="msapplication-TileColor" content="#603cba" />
         <meta name="theme-color" content="#0b73fe" />
 

73b4cbe842365640b0f12c19e1a76e65bc1c1ae5
maslomeister <maslomeister@gmail.com>
2022-11-22 18:05:26 +0400

    refactor: moved analytics

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -12,11 +12,6 @@ import logo from "public/assets/logo.svg";
 import mini_logo from "public/assets/logo_mini.svg";
 
 import styles from "./navbar.module.css";
-import Script from "next/script";
-import { YANDEX_METRIKA_ID } from "../../utils/yandex-metrika";
-import { GOOGLE_ANALYTICS_ID } from "../../utils/google-analytics";
-
-const analyticsEnabled = process.env.NODE_ENV === "production";
 
 export const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
@@ -43,50 +38,6 @@ export const Navbar = () => {
         <MenuItem url={urls.prices}>Цены и Услуги</MenuItem>
       </ul>
       <DynamicSidebar isOpen={isOpen} closeMenu={closeMenu} />
-      <div className={styles.analytics}>
-        {analyticsEnabled && (
-          <Script
-            id="yandex-metrica"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-  m[i].l=1*new Date();
-  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-  ym(${YANDEX_METRIKA_ID}, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-  });`,
-            }}
-          />
-        )}
-        {analyticsEnabled && (
-          <Image src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
-        )}
-        {analyticsEnabled && (
-          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`} />
-        )}
-
-        {analyticsEnabled && (
-          <Script
-            strategy="lazyOnload"
-            id="google-analytics"
-            dangerouslySetInnerHTML={{
-              __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', ${GOOGLE_ANALYTICS_ID}, {
-  page_path: window.location.pathname,
-  });`,
-            }}
-          />
-        )}
-      </div>
     </header>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import Image from "next/image";
+import { GOOGLE_ANALYTICS_ID } from "../utils/google-analytics";
+import { YANDEX_METRIKA_ID } from "../utils/yandex-metrika";
+
+const analyticsEnabled = process.env.NODE_ENV === "production";
 
 export default function Document() {
   return (
@@ -23,6 +29,48 @@ export default function Document() {
         <meta property="og:site_name" content="Безпересдач" />
       </Head>
       <body>
+        {analyticsEnabled && (
+          <Script
+            id="yandex-metrica"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  ym(${YANDEX_METRIKA_ID}, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+  });`,
+            }}
+          />
+        )}
+        {analyticsEnabled && (
+          <Image src={`https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}`} style={{ position: "absolute", left: "-9999px" }} alt="" />
+        )}
+        {analyticsEnabled && (
+          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`} />
+        )}
+
+        {analyticsEnabled && (
+          <Script
+            strategy="lazyOnload"
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', ${GOOGLE_ANALYTICS_ID}, {
+  page_path: window.location.pathname,
+  });`,
+            }}
+          />
+        )}
         <Main />
         <NextScript />
       </body>

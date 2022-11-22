238e68bcaf9c38cc57d3297f53a4cdcb93649797
maslomeister <maslomeister@gmail.com>
2022-11-23 03:28:26 +0400

    feat: log hostname and captcha

--- pages/api/new-order-form-api.ts
+++ pages/api/new-order-form-api.ts
@@ -42,6 +42,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       const captchaValidation = await data;
 
+      console.log(hostname, captchaValidation);
+
       if (captchaValidation.success === captchaSuccess && captchaValidation.hostname === hostname) {
         await createOrder(order);
 

d6fdbfccca867dd7b79744c7d1894ee5c1c0e97d
maslomeister <maslomeister@gmail.com>
2022-11-22 19:51:37 +0400

    feat: change color of loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <NextNProgress height={6} showOnShallow={false} options={{ showSpinner: false }} />
+      <NextNProgress color="#1070EE" height={6} showOnShallow={false} options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </ErrorBoundary>
   );

c9ecc9ac9a6d4e3e02b0f83aba680508b0d8937e
maslomeister <maslomeister@gmail.com>
2022-11-24 01:57:35 +0400

    feat: recaptcha disclaimer

---
+++ components/forms/components/recaptcha-disclaimer/recaptcha-disclaimer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+
+import RecaptchaLogo from "public/assets/images/recaptcha-logo.svg";
+
+import styles from "./recaptcha-disclaimer.module.css";
+
+interface Props {
+  color?: string;
+}
+
+export const RecaptchaDisclaimer = ({ color }: Props) => {
+  return (
+    <div className={styles.recaptcha} style={{ color }}>
+      <div className={styles.recaptcha_logo_text}>
+        <p>
+          Мы используем{" "}
+          <a href="https://www.google.com/recaptcha/about/" className={styles.link} target="_blank" rel="noreferrer">
+            reCAPTCHA
+          </a>
+        </p>
+        <a href="https://www.google.com/recaptcha/about/" target="_blank" rel="noreferrer">
+          <Image src={RecaptchaLogo} className={styles.recaptcha_logo} alt={"recaptcha logo"} />
+        </a>
+      </div>
+    </div>
+  );
+};

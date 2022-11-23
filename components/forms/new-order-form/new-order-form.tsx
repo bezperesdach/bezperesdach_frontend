9c8541b62b094e4dbc2cede72d43e7f44d2ec02c
maslomeister <maslomeister@gmail.com>
2022-11-23 04:19:49 +0400

    log response

--- components/forms/new-order-form/new-order-form.tsx
+++ components/forms/new-order-form/new-order-form.tsx
@@ -100,6 +100,8 @@ export const NewOrderForm = ({ projectType }: Props) => {
         },
       });
 
+      console.log(response);
+
       if (response.statusText === "OK") {
         setSendOrder((prevState) => {
           return { ...prevState, loading: false, isModal: true };

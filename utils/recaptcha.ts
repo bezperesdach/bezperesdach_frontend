0e662a9e3ba6650138ea9b899f8780ecf01afd98
maslomeister <maslomeister@gmail.com>
2022-11-24 01:58:19 +0400

    feat: verify recaptcha method

--- utils/recaptcha.ts
+++ utils/recaptcha.ts
@@ -1,3 +1,19 @@
-export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string;
+import axios from "axios";
 
-export const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY as string;
+export const RECAPTCHA_SITE_KEY =
+  process.env.NODE_ENV === "development"
+    ? (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_DEV as string)
+    : (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string);
+
+export const RECAPTCHA_SECRET_KEY =
+  process.env.NODE_ENV === "development"
+    ? (process.env.RECAPTCHA_SECRET_KEY_DEV as string)
+    : (process.env.RECAPTCHA_SECRET_KEY as string);
+
+export const verifyRecaptcha = async (token: string) => {
+  const secretKey = RECAPTCHA_SECRET_KEY;
+
+  const verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + token;
+
+  return await axios.post(verificationUrl);
+};

0d31617a8bf9de7ec4c77d29cb16daae531116d2
maslomeister <maslomeister@gmail.com>
2022-11-23 16:47:12 +0400

    refactor: hcaptcha -> recaptcha

--- pages/api/new-order-form-api.ts
+++ pages/api/new-order-form-api.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createOrder } from "../../api/api";
 
-const captchaSuccess = process.env.NODE_ENV === "development" ? false : true;
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body, method } = req;
 
@@ -20,34 +18,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
       const data = axios
-        .post(
-          "https://hcaptcha.com/siteverify",
-          {
-            response: captcha,
-            secret: process.env.HCAPTCHA_SECRET_KEY,
+        .post(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
           },
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-            },
-          }
-        )
+        })
         .then((res) => res.data);
 
       const captchaValidation = await data;
 
-      if (captchaValidation.success === captchaSuccess) {
+      if (captchaValidation.success) {
         await createOrder(order);
 
         return res.status(200).send("OK");
       }
 
       return res.status(422).json({
-        message: "Неверная каптча",
+        message: "Неверная Капча",
       });
     } catch (error) {
       console.log(error);
-      return res.status(422).json({ message: "Что-то пошло не так, повторите отправку" });
+      return res.status(422).json({ message: `Что-то пошло не так, повторите отправку: ${error}` });
     }
   }
   return res.status(404).send("Not found");
---
+++ utils/recaptcha.ts
@@ -0,0 +1,3 @@
+export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string;
+
+export const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY as string;

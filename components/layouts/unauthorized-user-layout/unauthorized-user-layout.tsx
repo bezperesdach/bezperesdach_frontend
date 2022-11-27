644931bc2717dec65e18a5d02c2fafe13328da6c
maslomeister <maslomeister@gmail.com>
2022-11-27 23:18:15 +0400

    refactor: added missing imports

--- components/layouts/unauthorized-user-layout/unauthorized-user-layout.tsx
+++ components/layouts/unauthorized-user-layout/unauthorized-user-layout.tsx
@@ -1,12 +1,14 @@
 import dynamic from "next/dynamic";
 import type { ReactNode } from "react";
+const DynamicNavbar = dynamic(() => import("../../navbar/navbar").then((mod) => mod.Navbar));
+const DynamicFooter = dynamic(() => import("../../footer/footer").then((mod) => mod.Footer));
 
 import styles from "./unauthorized-user-layout.module.css";
 
 export const UnauthorizedUserLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className={styles.main}>
-      <Navbar />
+      <DynamicNavbar />
       <main>{children}</main>
       <DynamicFooter />
     </div>

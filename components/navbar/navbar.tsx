b95c9fe2bf66dcc8c77575b972721aeafb6d1742
Salavat N <i@snasyrov.ru>
2022-11-27 03:14:57 +0200

    edit: add new page on nevbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -36,6 +36,7 @@ export const Navbar = () => {
           Главная
         </MenuItem>
         <MenuItem url={urls.about_us}>О нас</MenuItem>
+        <MenuItem url={urls.garantees}>Гарантии</MenuItem>
         <MenuItem url={urls.prices}>Цены и Услуги</MenuItem>
       </ul>
       <DynamicSidebar isOpen={isOpen} closeMenu={closeMenu} />

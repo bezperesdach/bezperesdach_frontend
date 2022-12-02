2fdb8fc0b5aa4eb4ba9d54cea19f11adfd81d6cc
maslomeister <maslomeister@gmail.com>
2022-12-02 22:36:48 +0400

    cleanup

--- components/scroll-to-top-button/scroll-to-top-button.tsx
+++ components/scroll-to-top-button/scroll-to-top-button.tsx
@@ -4,6 +4,15 @@ import { useScroll } from "framer-motion";
 
 import styles from "./scroll-to-top-button.module.css";
 
+const containerVariants = {
+  hover: { opacity: 1 },
+};
+
+const buttonVariants = {
+  rest: { scale: 1 },
+  hover: { scale: 1.2 },
+};
+
 export const ScrollTopButton = () => {
   const { scrollY } = useScroll();
 
@@ -18,7 +27,7 @@ export const ScrollTopButton = () => {
 
   useEffect(() => {
     return scrollY.onChange((latest) => {
-      if (latest > 500) {
+      if (latest > 1000) {
         setVisible(true);
       } else {
         setVisible(false);
@@ -27,26 +36,32 @@ export const ScrollTopButton = () => {
   }, []);
 
   return (
-    <div className={styles.scroll_top_container}>
-      <AnimatePresence>
-        {visible && (
-          <motion.div
-            initial={{ y: "200%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "200%" }}
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 600, damping: 20, duration: 0.8 }}
-            className={styles.scroll_top_wrap}
-          >
-            <a role="button" aria-label="Scroll to top" onClick={scrollToTop}>
+    <AnimatePresence>
+      {visible && (
+        <motion.div
+          className={styles.scroll_top_wrap}
+          initial={{ y: "200%", opacity: 0.8 }}
+          animate={{ y: 0, opacity: 0.8 }}
+          exit={{ y: "200%", opacity: 0.8 }}
+          variants={containerVariants}
+          whileHover="hover"
+          transition={{ type: "ease" }}
+        >
+          <a role="button" aria-label="Scroll to top" onClick={scrollToTop}>
+            <motion.div
+              className={styles.svg_wrap}
+              initial="rest"
+              whileHover="hover"
+              variants={buttonVariants}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
               <svg viewBox="0 0 48 48" width="48px" height="48px" xmlns="http://www.w3.org/2000/svg">
-                <path id={styles.scroll_top_bg} d="M0 0h48v48h-48z"></path>
                 <path id={styles.scroll_top_arrow} d="M14.83 30.83l9.17-9.17 9.17 9.17 2.83-2.83-12-12-12 12z"></path>
               </svg>
-            </a>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+            </motion.div>
+          </a>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };

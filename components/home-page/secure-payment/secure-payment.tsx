ee0c386ef9dd65429e0ee6b6172f21fed1d80ab8
Nikita Maslov <maslomeister@gmail.com>
2022-12-30 15:23:54 +0400

    refactor: renamed

--- components/home-page/secure-payment/secure-payment.tsx
+++ components/home-page/secure-payment/secure-payment.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import { ImageWithText } from "../../image-with-text/image-with-text";
-import Main from "public/assets/images/save-money/1.webp";
-import FallbackMain from "public/assets/images/save-money/fallback-1.png";
+import Main from "public/assets/images/secure-payment/1.webp";
+import FallbackMain from "public/assets/images/secure-payment/fallback-1.png";
 
-import Cloud from "public/assets/images/save-money/2.webp";
-import FallbackCloud from "public/assets/images/save-money/fallback-2.png";
+import Payment from "public/assets/images/secure-payment/2.webp";
+import FallbackPayment from "public/assets/images/secure-payment/fallback-2.png";
 
-import styles from "./save-money.module.css";
+import styles from "./secure-payment.module.css";
 
-export const SaveMoney = () => {
+export const SecurePayment = () => {
   return (
     <section className={`${styles.save_money} ${styles.colored_background}`}>
       <div className={styles.cards}>
@@ -18,27 +18,27 @@ export const SaveMoney = () => {
         </h2>
         <ImageWithText
           description={"Не перечисляем деньги автору до конца сделки"}
-          img={Cloud}
-          fallbackImg={FallbackCloud}
+          img={Payment}
+          fallbackImg={FallbackPayment}
           alt="cloud point"
         />
         <ImageWithText
           description={"Работаем с платежной системой YoMoney"}
-          img={Cloud}
-          fallbackImg={FallbackCloud}
+          img={Payment}
+          fallbackImg={FallbackPayment}
           alt="cloud point"
         />
         <ImageWithText
           description={"Выдаем чек независимо от суммы заказа"}
-          img={Cloud}
-          fallbackImg={FallbackCloud}
+          img={Payment}
+          fallbackImg={FallbackPayment}
           alt="cloud point"
         />
 
         <ImageWithText
           description={"Авторские права переходят к вам согласно ТК РФ"}
-          img={Cloud}
-          fallbackImg={FallbackCloud}
+          img={Payment}
+          fallbackImg={FallbackPayment}
           alt="cloud point"
         />
       </div>

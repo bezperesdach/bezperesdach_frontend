16a4d804e41eec424fb18c84654ed77ad6ddbd00
Nikita Maslov <maslomeister@gmail.com>
2023-01-04 17:43:59 +0400

    feat: import mp4 functionality

--- types/types.d.ts
+++ types/types.d.ts
@@ -26,6 +26,10 @@ type VKGoalTypes =
   | "donate" // (рус. Пожертвование средств) — пожертвование средств.
   | "conversion"; // (рус. Конверсия) — конверсия, или целевое действия без уточнения подробностей. Используйте этот тип события, если в списке событий выше вы не нашли подходящее для вашего целевого действия.
 
+declare module "*.mp4" {
+  export default string;
+}
+
 interface IOrder {
   contactType: string;
   contact: string;

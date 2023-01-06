ebb9d4398d92fb5ef8b606df35e181f767662821
Nikita Maslov <maslomeister@gmail.com>
2023-01-07 00:44:19 +0400

    feat: animations for safari/chrome

--- components/home-page/our-advantages/components/card/card.tsx
+++ components/home-page/our-advantages/components/card/card.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { useState } from "react";
+import dynamic from "next/dynamic";
 import Image, { StaticImageData } from "next/image";
 import styles from "./card.module.css";
+import { boolean } from "yup";
+
+const DynamicCardVideo = dynamic(() => import("../../../video-component/video-component"));
 
 type Props = {
   title: string;
@@ -8,18 +12,34 @@ type Props = {
   img: string | StaticImageData;
   fallbackImg: StaticImageData;
   alt: string;
+  videoMov?: string;
+  videoWebm?: string;
 };
 
-export const Card = ({ title, children, img, fallbackImg, alt }: Props) => {
+export const Card = ({ title, children, img, fallbackImg, alt, videoMov, videoWebm }: Props) => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div className={styles.card_style}>
-      <Image
-        src={img}
-        placeholder="blur"
-        className={`${styles.image} no_select image_no_pointer_events`}
-        alt={alt}
-        onError={(e) => (e.currentTarget.src = fallbackImg.src)}
-      />
+      <div className={styles.image_container}>
+        <Image
+          src={img}
+          placeholder="blur"
+          className={`${styles.image} no_select image_no_pointer_events`}
+          alt={alt}
+          style={{ visibility: !isVisible ? "visible" : "hidden" }}
+          onError={(e) => (e.currentTarget.src = fallbackImg.src)}
+        />
+
+        {videoMov && (
+          <DynamicCardVideo
+            className={styles.animated_image}
+            videoMov={videoMov}
+            videoWebm={videoWebm}
+            isVisible={isVisible}
+            setIsVisible={setIsVisible}
+          />
+        )}
+      </div>
       <div className={styles.card_text}>
         <h3>{title}</h3>
         {children}

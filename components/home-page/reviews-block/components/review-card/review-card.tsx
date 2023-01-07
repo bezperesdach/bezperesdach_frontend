1200e69644bfe80f2c7899c3ce02035f9abff161
Nikita Maslov <maslomeister@gmail.com>
2023-01-08 01:24:02 +0400

    refactor: priority and static imports

--- components/home-page/reviews-block/components/review-card/review-card.tsx
+++ components/home-page/reviews-block/components/review-card/review-card.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 import { RatingStars } from "../../../../rating-stars/rating-stars";
+import f1 from "../../../../../public/assets/images/avatars/f1.webp";
+import f2 from "../../../../../public/assets/images/avatars/f2.webp";
+import f3 from "../../../../../public/assets/images/avatars/f3.webp";
+import f4 from "../../../../../public/assets/images/avatars/f4.webp";
+import f5 from "../../../../../public/assets/images/avatars/f5.webp";
+import m1 from "../../../../../public/assets/images/avatars/m1.webp";
+import m2 from "../../../../../public/assets/images/avatars/m2.webp";
+import m3 from "../../../../../public/assets/images/avatars/m3.webp";
+import m4 from "../../../../../public/assets/images/avatars/m4.webp";
+import m5 from "../../../../../public/assets/images/avatars/m5.webp";
 
 import styles from "./review-card.module.css";
 
@@ -12,14 +22,42 @@ type Props = {
   avatar: string;
 };
 
+const getAvatar = (name: string) => {
+  switch (name) {
+    case "f1":
+      return f1;
+    case "f2":
+      return f2;
+    case "f3":
+      return f3;
+    case "f4":
+      return f4;
+    case "f5":
+      return f5;
+    case "m1":
+      return m1;
+    case "m2":
+      return m2;
+    case "m3":
+      return m3;
+    case "m4":
+      return m4;
+    case "m5":
+      return m5;
+  }
+
+  return m1;
+};
+
 export const ReviewCard = ({ className, name, text, rating, date, avatar }: Props) => {
   return (
     <div className={className}>
       <div className={styles.avatar_name_rating_container}>
         <Image
-          src={`/assets/images/avatars/${avatar}.webp`}
+          src={getAvatar(avatar)}
           className={`${styles.image} no_select image_no_pointer_events`}
           alt="avatar image"
+          priority
           width={64}
           placeholder="blur"
           height={64}

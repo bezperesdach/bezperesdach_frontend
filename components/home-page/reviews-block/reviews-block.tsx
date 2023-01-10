cd5620a025465104962b3af887b0108512465aaa
Nikita Maslov <maslomeister@gmail.com>
2023-01-10 14:01:28 +0400

    refactor: SSR -> CSR

--- components/home-page/reviews-block/reviews-block.tsx
+++ components/home-page/reviews-block/reviews-block.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 import Carousel from "react-multi-carousel";
 import { ReviewCard } from "./components/review-card/review-card";
 
 import styles from "./reviews-block.module.css";
 
 import "react-multi-carousel/lib/styles.css";
+import { useEffect, useState } from "react";
 
 type Props = {
-  deviceType: string;
-  randomReviews: RandomReviews | null;
+  className?: string;
 };
 
 const responsive = {
@@ -28,12 +30,28 @@ const responsive = {
   },
 };
 
-export const ReviewsBlock = ({ randomReviews, deviceType }: Props) => {
-  if (!randomReviews || !randomReviews.reviews || (randomReviews.reviews && randomReviews.reviews.length <= 0)) {
+export const ReviewsBlock = ({ className }: Props) => {
+  const [randomReviews, setRandomReviews] = useState<RandomReviews | null>(null);
+
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/get-reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setRandomReviews(data.data);
+          setLoading(false);
+        }
+      });
+  }, []);
+
+  if (isLoading || randomReviews === undefined || randomReviews === null) {
     return null;
   }
+
   return (
-    <section className={`${styles.reviews_block} ${styles.colored_background}`}>
+    <section className={`${className} ${styles.reviews_block} ${styles.colored_background}`}>
       <h2>ОТЗЫВЫ</h2>
       <h3 className={styles.avg_rating}>
         Средняя оценка <span className={styles.avg_rating_score}>{randomReviews.avgRating}</span>
@@ -47,11 +65,9 @@ export const ReviewsBlock = ({ randomReviews, deviceType }: Props) => {
         swipeable={false}
         draggable={false}
         infinite
-        ssr
         containerClass={styles.carousel_container}
         itemClass={styles.review_container}
         responsive={responsive}
-        deviceType={deviceType}
       >
         {randomReviews.reviews.map((review) => {
           return (
